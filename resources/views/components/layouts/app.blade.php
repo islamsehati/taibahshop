da2d816ee2addebc466316ffb94ebfbadcac04f7
islamsehati <orangislamsehati@gmail.com>
2025-04-28 17:22:38 +0700

    fix glitch in dark mode

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
+
+        <script>
+            // This code should be added to <head>.
+            // It's used to prevent page load glitches.
+            const html = document.querySelector('html');
+            const isLightOrAuto = localStorage.getItem('hs_theme') === 'light' || (localStorage.getItem('hs_theme') === 'auto' && !window.matchMedia('(prefers-color-scheme: dark)').matches);
+            const isDarkOrAuto = localStorage.getItem('hs_theme') === 'dark' || (localStorage.getItem('hs_theme') === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+            if (isLightOrAuto && html.classList.contains('dark')) html.classList.remove('dark');
+            else if (isDarkOrAuto && html.classList.contains('light')) html.classList.remove('light');
+            else if (isDarkOrAuto && !html.classList.contains('dark')) html.classList.add('dark');
+            else if (isLightOrAuto && !html.classList.contains('light')) html.classList.add('light');
+        </script>
+        
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
